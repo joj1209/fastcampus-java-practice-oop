00d4385ac4aedb799f116045341bc440bace414d
bskim <joj1209@naver.com>
2024-06-13 17:34:38 +0900

    gradle project setup

---
+++ src/main/java/com/fastcampus/javapracticeoop/JavaPracticeOopApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.javapracticeoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPracticeOopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPracticeOopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastcampus/javapracticeoop/JavaPracticeOopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.javapracticeoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPracticeOopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
